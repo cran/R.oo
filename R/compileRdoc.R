fa4bba5e584625f6451e214b489d1eedd7219200
Henrik Bengtsson <henrikb@braju.com>
2013-07-01 00:00:00 +0000

    version 1.13.9

--- R/compileRdoc.R
+++ R/compileRdoc.R
@@ -10,9 +10,11 @@
 # @synopsis
 #
 # \arguments{
-#   \item{pkgname}{A @character string specifying the package name. 
+#   \item{pkgname}{A @character string specifying the package name.
 #     If @NULL, the package name is inferred from the DESCRIPTION file
 #     that is automatically search for in the subdirectories.}
+#   \item{path}{A @character string specifying the path of the
+#     package source directory.}
 #   \item{...}{Additional arguments passed to \code{Rdoc\$compile()}.}
 #   \item{verbose}{If @TRUE, verbose output is printed, otherwise not.}
 # }
@@ -22,7 +24,7 @@
 # }
 #
 # \details{
-#   To compile all Rdoc comments in a package 'PkgA' that is located 
+#   To compile all Rdoc comments in a package 'PkgA' that is located
 #   under the current working directory (e.g. \code{PkgA/R/*.R}) from
 #   the system command line, do:
 #   \preformatted{
@@ -39,8 +41,8 @@
 # @keyword documentation
 # @keyword IO
 # @keyword internal
-#*/########################################################################### 
-compileRdoc <- function(pkgname=NULL, ..., verbose=TRUE) {
+#*/###########################################################################
+compileRdoc <- function(pkgname=NULL, path=pkgname, ..., verbose=TRUE) {
   require("R.oo") || stop("Package not loaded: R.oo");
 
   # Infer package name from DESCRIPTION?
@@ -66,23 +68,23 @@ compileRdoc <- function(pkgname=NULL, ..., verbose=TRUE) {
     }
   }
 
-  dir <- pkgname;
-  if (!file_test("-d", dir)) {
-    throw("No such package directory: ", dir);
+  if (is.null(path)) {
+    path <- pkgname;
   }
 
-  path <- file.path(dir, "R")
   if (!file_test("-d", path)) {
-    throw("No such package R/ directory: ", path);
+    throw("No such package directory: ", path);
   }
+  pathR <- file.path(path, "R")
+  if (!file_test("-d", pathR)) {
+    throw("No such package R/ directory: ", pathR);
+  }
+
 
   require(pkgname, character.only=TRUE) || throw("Package not loaded: ", pkgname);
 
-  opwd <- setwd(path);
+  opwd <- setwd(pathR);
   on.exit(setwd(opwd));
-
-  print(search());
-  print(sessionInfo());
 
   Rdoc$compile(..., verbose=verbose);
 } # compileRdoc()
@@ -90,6 +92,10 @@ compileRdoc <- function(pkgname=NULL, ..., verbose=TRUE) {
 
 ############################################################################
 # HISTORY:
+# 2013-05-30
+# o Added argument 'path' to compileRdoc().
+# 2013-04-03
+# o CLEANUP: compileRdoc() was outputting search() and sessionInfo().
 # 2013-03-08
 # o Created.
 ############################################################################
